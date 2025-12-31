2baf44d5ae08ce6719c6bf6d51984a7b2b123281
Johann <175734270+Jzaroli@users.noreply.github.com>
2025-12-31 10:37:02 -0800

    neurosymbolic project moved to new repo, added link to readme

--- Neurosymbolic_AI/client/src/utils/mutations.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const SUBMIT_PROFILE = gql`
-  mutation submitProfile($input: InferenceInput!) {
-    submitProfile(input: $input) {
-      placeholder
-    }  
-  } 
-`;
